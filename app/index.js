0297b70cdfbf3cac4c7121101618ac4a89614608
Glitch (sponge-sink) <none>
2018-02-21 21:57:46 +0000

    🐘🦂 Updated with Glitch

---
+++ app/index.js
@@ -0,0 +1,28 @@
+import expect from 'expect';
+
+const toggleTodo = (todo) => {
+  return {
+    ...todo,
+    completed: !todo.completed
+  };
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn redux',
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn redux',
+    completed: true
+  };
+  
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+};
+
+testToggleTodo();
+console.log('All tests passed.');
